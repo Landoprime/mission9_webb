46b648dcb84090be55823312567c90dc094077d2
Landoprime <Landonwebb44@gmail.com>
2024-03-07 16:54:12 -0700

    this is werid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import teamNames from "./CollegeBasketballTeams.json";
+
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function HeadingSection() {
+  return (
+    <>
+      <h1>Welcome to March Madness!!!!!!!!!!</h1>
+      <p>
+        This site will give all the details that we need in order to create a
+        perfect March Madness bracket.
+      </p>
+    </>
+  );
+}
+
+class TeamCard extends React.Component<Team> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h1>{oneTeam.school}</h1>
+        <h2>Mascot: {oneTeam.name}</h2>
+        <h3>
+          {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamNames.teams.map((team: Team, index: number) => (
+        <TeamCard key={index} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeadingSection />
+      <TeamList />
     </div>
   );
 }
